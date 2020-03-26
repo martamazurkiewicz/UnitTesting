e9e0dde397ee152bdcedb0479821c9f3a467428a
Michal <zawadzki.micha@gmail.com>
2020-03-26 15:31:57 +0100

    Wykonany test zgodnosci wieku

---
+++ UnitTesting/NUnitTests/MiiiZawadzki.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using App;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitTests
+{
+    [TestFixture]
+    public class MiiiZawadzki
+    {
+        [Test]
+        public void AgeEqualTest()
+        {
+            MainPageViewModel mainPageViewModel = new MainPageViewModel();
+            mainPageViewModel.Age = "18";
+            mainPageViewModel.SignIn.Execute("null");
+            Assert.AreEqual(mainPageViewModel.Age, mainPageViewModel.AgeOutput);
+        }
+    }
+}
